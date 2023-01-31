ac8a0a6386fb215e3e0746c36a20b96cfd52d5d2
Crypt.sol.codes <110722148+DanielEmmanuel1@users.noreply.github.com>
2023-01-31 10:32:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ const searchInput = document.querySelector('#searchInput');
 const searchBtn = document.querySelector('#searchBtn');
 const moviesList = document.querySelector('#moviesList');
 
-searchBtn.addEventListener('click', async () => {
-  const searchTerm = searchInput.value;
-  const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=your_api_key&query=${searchTerm}`);
-  const data = await response.json();
-  const movies = data.results;
-
-  movies.forEach(movie => {
-    const movieEl = document.createElement('li');
-    movieEl.innerHTML = `${movie.title} <button class="deleteBtn">Delete</button>`;
-
-    movieEl.querySelector('.deleteBtn').addEventListener('click', () => {
-      movieEl.remove();
+if (searchInput && searchBtn && moviesList) {
+  searchBtn.addEventListener('click', async () => {
+    const searchTerm = searchInput.value;
+    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=f32b07c54012b068e434f26d8a0495b1&query=${searchTerm}`);
+    const data = await response.json();
+    const movies = data.results;
+  
+    movies.forEach(movie => {
+      const movieEl = document.createElement('li');
+      movieEl.innerHTML = `${movie.title} <button class="deleteBtn">Delete</button>`;
+  
+      movieEl.querySelector('.deleteBtn').addEventListener('click', () => {
+        movieEl.remove();
+      });
+  
+      moviesList.appendChild(movieEl);
     });
-
-    moviesList.appendChild(movieEl);
   });
-});
+}
